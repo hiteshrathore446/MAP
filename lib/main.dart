1a5967cf49d379f27f7594435bfd282fd4cf3bea
HITESH RATHORE <hvr446@gmail.com>
2024-09-10 21:51:58 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
 
-
-
-
-
-
-
-
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MapScreen(),
